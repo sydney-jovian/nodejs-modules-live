b08122d73af5ac2e8382815b5e1a57ad231fa53d
sydney-jovian <126050345+sydney-jovian@users.noreply.github.com>
2023-03-16 15:33:48 +0000

    Create a math module

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const math = require("./math.js");
+
 let num1 = parseInt(process.argv[2]);
 let num2 = parseInt(process.argv[3]);
-let result = num1 + num2;
+let result = math.add(num1, num2);
 console.log(`Sum of numbers ${num1} and ${num2} is ${result}`);
---
+++ math.js
@@ -0,0 +1,10 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+exports.add = add
+exports.subtract = subtract
